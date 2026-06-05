6f2ec69e81b0f4cf0a2dbc3b1c9ecd08af4b4753
Tiago-burlamaque <tiagoburlamaque79@gmail.com>
2026-06-05 17:15:04 -0300

    atualizando banco de dados

--- Database/db.sql
+++ Database/db.sql
@@ -2,18 +2,20 @@ CREATE SCHEMA IF NOT EXISTS smartHealth_city;
 
 USE smartHealth_city;
 
-CREATE TABLE avaliacoes(
+CREATE TABLE aluno (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    turma INT NOT NULL
+);
+
+CREATE TABLE avaliacoes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    
+
     horas_celular INT,
     horas_sono INT,
-
     atividade_fisica BOOLEAN,
-
     horas_sentado INT,
-
     area_verde BOOLEAN,
-
     celular_antes_dormir BOOLEAN,
 
     nivel_sedentarismo VARCHAR(50),
@@ -21,5 +23,11 @@ CREATE TABLE avaliacoes(
     saude_postural VARCHAR(50),
     bem_estar_mental VARCHAR(50),
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    aluno_id INT NOT NULL,
+
+    pontuacao INT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (aluno_id) REFERENCES aluno(id)
 );
