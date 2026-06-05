33760b51016412d6669efb9ba44661238e39e3ac
Tiago-burlamaque <tiagoburlamaque79@gmail.com>
2026-06-05 16:53:59 -0300

    adicionando rota do projeto

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: `smarthealth-city-production.up.railway.app:${process.env.PORT}`
 });
 
 export default api;
